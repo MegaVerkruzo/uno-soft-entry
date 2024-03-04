8b791695137ef8e294d7b6287ff371ac5ffafe73
Alexey Grunskii <robochat@mail.ru>
2024-03-04 15:14:45 +0300

    Make read

--- src/main/java/org/example/Backend.java
+++ src/main/java/org/example/Backend.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Backend implements Runnable {
     private final Path inputFilePath;
@@ -19,10 +21,17 @@ public class Backend implements Runnable {
 
     @Override
     public void run() {
-        long start = System.currentTimeMillis();
-        Map<Integer, List<String>> input = loadFile(inputFilePath);
+        final long start = System.currentTimeMillis();
+        final List<String[]> inputRows;
+        try {
+            inputRows = loadFile(inputFilePath);
+        } catch (IOException e) {
+            System.err.println("Something incorrect with I/O: " + e);
+            System.exit(1);
+            return;
+        }
 
-        List<List<String>> result = getResult(input);
+        List<List<String>> result = transformInputToResult(inputRows);
         long resultGroupsAmount = getGroupsAmountWithMoreThanOneNode(result);
         writeFile(outputFilePath, result, resultGroupsAmount);
 
@@ -37,14 +46,33 @@ public class Backend implements Runnable {
                 .count();
     }
 
-    private static List<List<String>> getResult(Map<Integer, List<String>> input) {
+    private static List<List<String>> transformInputToResult(List<String[]> input) {
         // :TODO
         return null;
     }
 
-    private static Map<Integer, List<String>> loadFile(Path filePath) {
-        // :TODO
-        return null;
+    private static List<String[]> loadFile(Path filePath) throws IOException {
+        List<String> allLines = Files.readAllLines(filePath);
+        List<String[]> result = new ArrayList<>(allLines.size());
+        for (String line : allLines) {
+            String[] row = line.split(";");
+            boolean isCorrectRow = true;
+            for (String columnWithSpaces : row) {
+                String column = columnWithSpaces.trim();
+                if (column.length() < 2
+                        || (column.charAt(0) != '"' || column.charAt(column.length() - 1) != '"')
+                        || column.substring(1, column.length() - 1).contains("\"")
+                ) {
+                    isCorrectRow = false;
+                    break;
+                }
+            }
+
+            if (isCorrectRow) {
+                result.add(row);
+            }
+        }
+        return result;
     }
 
     private static void writeFile(Path filePath, List<List<String>> result, long resultGroupsAmount) {

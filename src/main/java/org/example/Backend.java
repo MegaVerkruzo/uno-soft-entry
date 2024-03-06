51ac46a96ac6ba034ba8a97016fab2dcb6b2a275
Alexey Grunskii <robochat@mail.ru>
2024-03-06 11:19:50 +0300

    Finish with code

--- src/main/java/org/example/Backend.java
+++ src/main/java/org/example/Backend.java
@@ -1,10 +1,20 @@
 package org.example;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class Backend implements Runnable {
     private final Path inputFilePath;
@@ -31,24 +41,22 @@ public class Backend implements Runnable {
             return;
         }
 
-        List<List<String>> result = transformInputToResult(inputRows);
+        TreeMap<KeySizeParent, Set<String>> result = transformInputToResult(inputRows);
         long resultGroupsAmount = getGroupsAmountWithMoreThanOneNode(result);
-        writeFile(outputFilePath, result, resultGroupsAmount);
+        try {
+            writeFile(outputFilePath, result, resultGroupsAmount);
+        } catch (IOException e) {
+            System.err.println("Thrown exception while making print output: + " + e);
+            System.exit(1);
+            return;
+        }
 
         long endTime = System.currentTimeMillis();
         printMessageResults(resultGroupsAmount, start, endTime);
     }
 
-    private static long getGroupsAmountWithMoreThanOneNode(List<List<String>> result) {
-        return result
-                .stream()
-                .filter(list -> list.size() > 1)
-                .count();
-    }
-
-    private static List<List<String>> transformInputToResult(List<String[]> input) {
-        // :TODO
-        return null;
+    private static long getGroupsAmountWithMoreThanOneNode(TreeMap<KeySizeParent, Set<String>> result) {
+        return result.keySet().stream().filter(key -> key.size > 1).count();
     }
 
     private static List<String[]> loadFile(Path filePath) throws IOException {
@@ -75,12 +83,88 @@ public class Backend implements Runnable {
         return result;
     }
 
-    private static void writeFile(Path filePath, List<List<String>> result, long resultGroupsAmount) {
-        // :TODO
+    private static TreeMap<KeySizeParent, Set<String>> transformInputToResult(List<String[]> input) {
+        List<Node> dsu = getNodes(input);
+        Map<Integer, List<Node.NodeElement>> anotherRepresentation = getColumnWordsMap(input, dsu);
+        unionNodesByDSU(anotherRepresentation);
+
+        TreeMap<KeySizeParent, Set<String>> resultGroup = new TreeMap<>(KeySizeParent.comparator);
+        dsu.forEach(node -> {
+            KeySizeParent key = new KeySizeParent(node.getSize(), Node.getParent(node).getNumber());
+            resultGroup.putIfAbsent(key, new HashSet<>());
+            resultGroup.get(key).add(node.getRow());
+        });
+        return resultGroup;
+    }
+
+    private static void unionNodesByDSU(Map<Integer, List<Node.NodeElement>> anotherRepresentation) {
+        for (Map.Entry<Integer, List<Node.NodeElement>> entry : anotherRepresentation.entrySet()) {
+            Map<String, Node.NodeElement> map = new HashMap<>();
+            for (final Node.NodeElement element : entry.getValue()) {
+                if (!map.containsKey(element.getData())) {
+                    map.put(element.getData(), element);
+                    continue;
+                }
+
+                Node.unionNodes(
+                        map.get(element.getData()).getParentNode(),
+                        element.getParentNode()
+                );
+            }
+        }
+    }
+
+    private static List<Node> getNodes(List<String[]> input) {
+        List<Node> snm = new ArrayList<>(input.size());
+        for (int i = 0; i < input.size(); ++i) {
+            snm.add(Node.createNode(String.join(";", input.get(i)), i));
+        }
+        return snm;
+    }
+
+    private static Map<Integer, List<Node.NodeElement>> getColumnWordsMap(List<String[]> input, List<Node> snm) {
+        Map<Integer, List<Node.NodeElement>> anotherRepresentation = new HashMap<>();
+        for (int i = 0; i < input.size(); ++i) {
+            String[] currentRow = input.get(i);
+            for (int column = 0; column < currentRow.length; ++column) {
+                Objects.requireNonNull(anotherRepresentation.putIfAbsent(column, List.of()))
+                        .add(Node.NodeElement.createElement(
+                                currentRow[column].substring(1, currentRow[column].length() - 1), snm.get(i)
+                        ));
+            }
+        }
+        return anotherRepresentation;
+    }
+
+    private static void writeFile(Path filePath, TreeMap<KeySizeParent, Set<String>> result, long resultGroupsAmount) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(filePath, StandardCharsets.UTF_8,
+                StandardOpenOption.TRUNCATE_EXISTING,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.CREATE_NEW
+        )) {
+            writer.write(resultGroupsAmount + "\n");
+            int groupNumber = 1;
+            for (Map.Entry<KeySizeParent, Set<String>> group : result.entrySet()) {
+                writer.write("Группа " + (groupNumber++) + "\n");
+                for (String row : group.getValue()) {
+                    writer.write(row + "\n");
+                }
+                writer.newLine();
+            }
+        }
     }
 
     private static void printMessageResults(long resultGroupsAmount, long startTime, long endTime) {
         System.out.println("Groups with more than one " + resultGroupsAmount);
         System.out.println("Time in millis is " + (endTime - startTime));
+    }
+
+    private record KeySizeParent(int size, int parentNumber) {
+
+        // in desc order by size
+        // then asc order by parentNumber
+        private static final Comparator<KeySizeParent> comparator = (o1, o2) -> (o1.size != o2.size)
+                ? o2.size - o1.size
+                : o1.parentNumber - o2.parentNumber;
     }
 }
---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,90 @@
+package org.example;
+
+public class Node {
+    private final String row;
+    private final int number;
+    private int size = 1;
+    private Node parent = null;
+
+    private Node(String row, int number) {
+        this.row = row;
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getSize() {
+        return getParent(this).size;
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+    public static void unionNodes(Node left, Node right) {
+        Node topLeft = getParent(left);
+        Node topRight = getParent(right);
+
+        if (topLeft.number == topRight.number) {
+            return;
+        }
+
+        if (topLeft.number < topRight.number) {
+            topRight.parent = topLeft;
+            topLeft.size += topRight.size;
+        } else {
+            topLeft.parent = topRight;
+            topRight.size += topLeft.size;
+        }
+    }
+
+    public static Node getParent(Node node) {
+        Node parent;
+        if (node.parent != null) {
+            parent = getParent(node.parent);
+        } else {
+            parent = node;
+        }
+
+        node.parent = parent;
+        return node;
+    }
+
+    public static Node createNode(String src, int number) {
+        return new Node(src, number);
+    }
+
+    public static class NodeElement {
+        private final String data;
+        private final Node parentNode;
+
+        private NodeElement(String data, Node parentNode) {
+            this.data = data;
+            this.parentNode = parentNode;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public Node getParentNode() {
+            return parentNode;
+        }
+
+        public static NodeElement createElement(String data, Node parentNode) {
+            return new NodeElement(data, parentNode);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            NodeElement that = (NodeElement) o;
+
+            return data.equals(that.data);
+        }
+    }
+}
